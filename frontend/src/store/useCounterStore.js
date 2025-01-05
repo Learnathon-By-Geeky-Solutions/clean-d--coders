ec946c3368b094ca15506673515ee2cc58851f89
SaiemAziz <sayemazizchy@gmail.com>
2025-01-05 16:39:07 +0600

    docker set up, included render live link

--- frontend/src/store/useCounterStore.js
+++ frontend/src/store/useCounterStore.js
@@ -4,15 +4,15 @@ const useCounterStore = create((set) => ({
   setCounter: (val) => set({ counter: val }),
   increment: () =>
     set((state) => {
-      if (state.counter === 100) {
-        alert("Counter cannot be more than 5");
+      if (state.counter >= 100) {
+        alert("Counter cannot be more than 100");
         return state;
       }
       return { counter: state.counter + 1 };
     }),
   decrement: () =>
     set((state) => {
-      if (state.counter === 0) {
+      if (state.counter <= 0) {
         alert("Counter cannot be less than 0");
         return state;
       }
