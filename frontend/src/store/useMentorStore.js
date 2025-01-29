51408fdb63a24af0587ef78d5685395f0fae73eb
SaiemAziz <sayemazizchy@gmail.com>
2025-01-29 23:31:09 +0600

    introduced get method in zustand store

--- frontend/src/store/useMentorStore.js
+++ frontend/src/store/useMentorStore.js
@@ -1,30 +1,30 @@
 import { apiAllMentors } from "@/utils";
 import { create } from "zustand";
 const filterByCompanies = (mentors, companies) => {
-    if (companies[0] === "all") return mentors;
-    return mentors.filter((m) => companies.includes(m.company));
-  };
-  
-  const filterByUniversities = (mentors, universities) => {
-    if (universities[0] === "all") return mentors;
-    return mentors.filter((m) => universities.includes(m.university));
-  };
-  
-  const filterByCosts = (mentors, costs) => {
-    if (costs[0] === "all") return mentors;
-    return mentors.filter((m) => {
-      return (
-        (m.cost === 0 && costs.includes("unpaid")) ||
-        (m.cost !== 0 && costs.includes("paid"))
-      );
-    });
-  };
-  
-  const filterByTags = (mentors, tags) => {
-    if (tags[0] === "all") return mentors;
-    return mentors.filter((m) => tags.some((tt) => m.tags.includes(tt)));
-  };
-const useMentorStore = create((set) => ({
+  if (companies[0] === "all") return mentors;
+  return mentors.filter((m) => companies.includes(m.company));
+};
+
+const filterByUniversities = (mentors, universities) => {
+  if (universities[0] === "all") return mentors;
+  return mentors.filter((m) => universities.includes(m.university));
+};
+
+const filterByCosts = (mentors, costs) => {
+  if (costs[0] === "all") return mentors;
+  return mentors.filter((m) => {
+    return (
+      (m.cost === 0 && costs.includes("unpaid")) ||
+      (m.cost !== 0 && costs.includes("paid"))
+    );
+  });
+};
+
+const filterByTags = (mentors, tags) => {
+  if (tags[0] === "all") return mentors;
+  return mentors.filter((m) => tags.some((tt) => m.tags.includes(tt)));
+};
+const useMentorStore = create((set, get) => ({
   mentors: [],
   allMentors: [],
   isLoading: true,
@@ -65,37 +65,29 @@ const useMentorStore = create((set) => ({
     });
   },
   setFilteredMentor: (filter) => {
-    set((state) => {
-      const { companies, costs, tags, universities } = filter;
-      let newMentors = [...state.allMentors];
-      newMentors = filterByCompanies(newMentors, companies);
-      newMentors = filterByUniversities(newMentors, universities);
-      newMentors = filterByCosts(newMentors, costs);
-      newMentors = filterByTags(newMentors, tags);
-      return { ...state, mentors: newMentors };
-    });
+    const prev = get();
+    const { companies, costs, tags, universities } = filter;
+    let newMentors = [...prev.allMentors];
+    newMentors = filterByCompanies(newMentors, companies);
+    newMentors = filterByUniversities(newMentors, universities);
+    newMentors = filterByCosts(newMentors, costs);
+    newMentors = filterByTags(newMentors, tags);
+    set((state) => ({ ...state, mentors: newMentors }));
   },
-  setUnFillteredMentor: () =>
+  setUnFillteredMentor: () => {
+    const prev = get();
+    prev.setMentors();
+  },
+
+  setSortedMentors: (sortByCost) => {
+    const prev = get();
+    let newMentors = [...prev.mentors];
+    if (sortByCost) newMentors.sort((a, b) => b.cost - a.cost);
+    else newMentors.sort((a, b) => a.cost - b.cost);
     set((state) => ({
       ...state,
-      mentors: state.allMentors,
-      filterData: {
-        companies: ["all"],
-        universities: ["all"],
-        costs: ["all"],
-        tags: ["all"],
-      },
-    })),
-  setSortedMentors: (sortByCost) => {
-    set(state => {
-        let newMentors = [...state.mentors]
-        if(sortByCost) newMentors.sort((a, b) => b.cost - a.cost)
-        else newMentors.sort((a, b) => a.cost - b.cost)
-        return {
-            ...state,
-            mentors: newMentors
-        } 
-    })
+      mentors: newMentors,
+    }));
   },
 }));
 export default useMentorStore;
