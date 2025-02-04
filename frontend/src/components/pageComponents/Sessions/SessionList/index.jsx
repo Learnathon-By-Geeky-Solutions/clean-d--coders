5914e4892c99c4bf392d015dd8d0cf65ecc603f8
Sahim32 <sahimsalem@gmail.com>
2025-02-04 18:58:08 +0600

    toggle list style updated

--- frontend/src/components/pageComponents/Sessions/SessionList/index.jsx
+++ frontend/src/components/pageComponents/Sessions/SessionList/index.jsx
@@ -3,40 +3,42 @@ import GoalList from "./GoalList";
 import { useState } from "react";
 import { ChevronRight } from "lucide-react";
 
-
-
 const SessionList = ({ users, role }) => {
   const [expandedIndex, setExpandedIndex] = useState(null);
-  
+
   return (
     <div className="w-full max-w-full mt-5">
       <ul className="bg-gray-100 rounded-md overflow-hidden">
         {users.map((user, index) => (
           <li key={user.name + index} className="mb-2 last:mb-0">
             <button
-              className="w-full bg-gray-200 p-3 flex items-center cursor-pointer"
-              onClick={() => setExpandedIndex(expandedIndex === index ? null : index)}
+              className={`w-full p-3 flex items-center cursor-pointer transition-colors duration-300 ${
+                expandedIndex === index
+                  ? "bg-blue-200"
+                  : "bg-gray-200 hover:bg-gray-300"
+              }`}
+              onClick={() =>
+                setExpandedIndex(expandedIndex === index ? null : index)
+              }
               aria-expanded={expandedIndex === index}
             >
               <ChevronRight
-                className={`mr-2 h-4 w-4 transition-transform ${
-                  expandedIndex === index ? 'rotate-90' : ''
+                className={`mr-2 h-4 w-4 transition-transform duration-300 ${
+                  expandedIndex === index ? "rotate-90" : ""
                 }`}
               />
               {user.name}
             </button>
             {expandedIndex === index && (
-              <div className="pl-4 py-2 bg-white">
-                <GoalList goals={user.goals} role={role}/>
-              </div>
+             <div className="pl-4 py-2 bg-white border-l-4 border-blue-200 shadow-md">
+             <GoalList goals={user.goals} role={role} />
+           </div>
             )}
           </li>
         ))}
       </ul>
     </div>
   );
-
-
 };
 
 SessionList.propTypes = {
