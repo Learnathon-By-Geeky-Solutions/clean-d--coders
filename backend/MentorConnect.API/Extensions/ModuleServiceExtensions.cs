460dcc53e3ab714319afee13f3404516690a70c7
SaiemAziz <sayemazizchy@gmail.com>
2025-02-16 09:19:15 +0600

    Mediatr set up and tested initially

--- backend/MentorConnect.API/Extensions/ModuleServiceExtensions.cs
+++ backend/MentorConnect.API/Extensions/ModuleServiceExtensions.cs
@@ -1,6 +1,8 @@
 using System;
 using MentorConnect.Users.API.Extensions;
 using MentorConnect.Goals.API.Extensions;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 
 namespace MentorConnect.API.Extensions;
 
@@ -11,6 +13,20 @@ public static class ModuleServiceExtensions
         services.AddUserModule(config);
         services.AddGoalModule(config);
 
+        return services;
+    }
+
+    public static IServiceCollection AddModuleControllers(this IServiceCollection services)
+    {
+        var mvcBuilder = services.AddControllers();
+        var assemblies = Directory.GetFiles(AppContext.BaseDirectory, "*.API.dll", SearchOption.AllDirectories)
+                                  .Select(Assembly.LoadFrom)
+                                  .ToList();
+
+        foreach (var assembly in assemblies)
+        {
+            mvcBuilder.PartManager.ApplicationParts.Add(new AssemblyPart(assembly));
+        }
         return services;
     }
 }
